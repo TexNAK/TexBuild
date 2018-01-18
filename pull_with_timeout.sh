7aed4a9cc0bc43b43de9b15998ff75d129e7a1f1
Til Blechschmidt <til@blechschmidt.de>
2018-01-18 11:37:37 +0100

    Added script to pull docker container with timeout

---
+++ pull_with_timeout.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+docker pull blang/latex &
+BPID=$!
+i=0
+while True; do
+    if ps -p $BPID >&-; then
+        echo ".\c"
+        sleep 5
+        ((i+=5))
+
+        if (( i > 1200 )); then
+            echo " TIMEOUT!"
+            exit 1
+        fi
+    else
+        echo " done"
+        break
+    fi
+done
